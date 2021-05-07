f55c73183ce76bfb3fa60ea4bd6cfceeb3ecd0c1
debian-openbox <77241939+debian-openbox@users.noreply.github.com>
2021-05-07 18:37:38 +0200

    Update non-free.sh

--- scripts/non-free.sh
+++ scripts/non-free.sh
@@ -2,14 +2,14 @@
 #
 # Writes sources.list in order to add non-free repository
 
-echo "# deb http://http.debian.net/debian $DEBIAN_RELEASE main" > /etc/apt/sources.list
+echo "# deb http://http.debian.net/debian bullseye main" > /etc/apt/sources.list
 echo "" >> /etc/apt/sources.list
 echo "deb http://deb.debian.org/debian/ bullseye main contrib non-free" >> /etc/apt/sources.list
 echo "deb-src http://deb.debian.org/debian/ bullseye main contrib non-free" >> /etc/apt/sources.list
 echo "" >> /etc/apt/sources.list
+echo "# bullseye-updates, previously known as "volatile"" >> /etc/apt/sources.list
 echo "deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free" >> /etc/apt/sources.list
 echo "deb-src http://deb.debian.org/debian/ bullseye-updates main contrib non-free" >> /etc/apt/sources.list
 echo "" >> /etc/apt/sources.list
-echo "# $DEBIAN_RELEASE-updates, previously known as "volatile"" >> /etc/apt/sources.list
 echo "deb http://deb.debian.org/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list
 echo "deb-src http://deb.debian.org/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list
